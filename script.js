c8cf7e3e64eea5d2fc5cb6b19be1a449256c540f
Nikolay <72186929+Nikolay-Shlyapnikov@users.noreply.github.com>
2023-01-23 15:23:34 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const headerBurgerButton = document.querySelector('.page-header__burger'),
-    headerNav = document.querySelector('.page-header__nav'),
-    headerLogo = document.querySelector('.page-header__logo');
-
-headerBurgerButton.addEventListener('click', (e) => {
-    headerBurgerButton.classList.toggle('active');
-    headerNav.classList.toggle('active');
-    headerLogo.classList.toggle('active');
-});
